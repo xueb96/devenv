984c0e703f41f3ea2d259eaca279dfeb90487fb4
xueb96 <594411763@qq.com>
2023-05-10 10:48:07 +0800

    minio: default root credentials

--- minio/run.sh
+++ minio/run.sh
@@ -2,8 +2,8 @@
 
 docker run -dit --name minio \
 --restart unless-stopped \
--e MINIO_ROOT_USER=root \
--e MINIO_ROOT_PASSWORD=123456 \
+-e MINIO_ROOT_USER=minioadmin \
+-e MINIO_ROOT_PASSWORD=minioadmin \
 -p 9000:9000 \
 -p 9001:9001 \
 -v $PWD/data:/data \
